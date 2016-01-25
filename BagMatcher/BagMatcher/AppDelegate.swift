c8bbbca053c0859a5ae5b792d0fcc0e65e02befa
Sridhar <sridhar@sridhar-gurivireddys-macbook-pro.local>
2016-01-25 15:49:43 -0800

    BagMatcher - Just arragned views horribly with all buttons

---
+++ BagMatcher/BagMatcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BagMatcher
+//
+//  Created by Sridhar on 1/25/16.
+//  Copyright © 2016 sridhar.gureen.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BagMatcher/BagMatcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BagMatcher
+//
+//  Created by Sridhar on 1/25/16.
+//  Copyright © 2016 sridhar.gureen.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet var pathToCDF: NSTextField!
+    @IBOutlet var pathToTrace: NSTextField!
+    @IBOutlet var pathToVideo: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
