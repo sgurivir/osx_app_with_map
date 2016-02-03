5ed62728c1c763b9b5c41167520c0e518d99076d
Sridhar Gurivireddy <sridhar@sridhar-gurivireddys-macbook-pro.local>
2016-02-02 22:50:17 -0800

    Add KVO for media player

--- BagMatcher/BagMatcher/ViewController.swift
+++ BagMatcher/BagMatcher/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: NSViewController {
 
     @IBOutlet var VideoViewController: AVPlayerView!
     @IBOutlet var MapViewController: MKMapView!
+    var player: AVPlayer!
+    var _timeObserver:AnyObject?
+    private var myContext = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,27 +36,76 @@ class ViewController: NSViewController {
         print("Video = \(pathToVideo.stringValue)")
         
         playVideo()
-        updateMap()
+        updateMap(21.282778, long:-157.829444)
     }
     
+    // AVplayer
     func playVideo() {
         //Make a URL from your path
         _ = NSURL.fileURLWithPath(pathToVideo.stringValue)
-        let url2 = NSURL.fileURLWithPath("/tmp/im.mp4")
-        let player = AVPlayer(URL: url2)
+        let url2 = NSURL.fileURLWithPath(pathToVideo.stringValue)
+        
+        player = AVPlayer(URL: url2)
         VideoViewController.player = player
+        startObservers()
         player.play()
     }
     
-    func updateMap() {
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-        let regionRadius: CLLocationDistance = 1000
+    // MapView
+    func updateMap(lat: CLLocationDegrees, long: CLLocationDegrees) {
+        let initialLocation = CLLocation(latitude: lat, longitude: long)
+        let regionRadius: CLLocationDistance = 500
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
         func centerMapOnLocation(location: CLLocation) {
             let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                regionRadius * 2.0, regionRadius * 2.0)
+                regionRadius * 1.0, regionRadius * 1.0)
             MapViewController.setRegion(coordinateRegion, animated: true)
         }
         centerMapOnLocation(initialLocation)
+    }
+    
+    func positionUpdated(seconds: Float64) {
+        print(formattedTimeForInterval(seconds))
+       // _positionSlider.value = Float(seconds / _currentTrackDuration);
+    }
+    
+    func formattedTimeForInterval(interval: NSTimeInterval) -> String {
+        let min = NSInteger(interval / 60)
+        let sec = NSInteger(interval % 60)
+        
+        return NSString(format: "%d:%02d", min, sec) as String
+    }
+    
+    func startObservers() {
+        if (_timeObserver == nil) {
+            _timeObserver = player.addPeriodicTimeObserverForInterval(CMTimeMake(1, 100), queue: dispatch_get_main_queue(),
+                usingBlock: { (time: CMTime) -> Void in
+                    let seconds:Float64 = CMTimeGetSeconds(time)
+                    if (!isnan(seconds) && !isinf(seconds)) {
+                        self.positionUpdated(seconds)
+                    }
+            })
+        }
+        //player.addObserver(self, forKeyPath: "status", options: .New, context: &myContext)
+        //player.addObserver(self, forKeyPath: "loadedTimeRanges", options: .New, context:  &myContext)
+        //player.addObserver(self, forKeyPath: "AVPlayerItemDidPlayToEndTimeNotification", options: .New, context:  &myContext)
+        
+    }
+    
+    func stopObservers() {
+        if (_timeObserver != nil) {
+            player.removeTimeObserver(_timeObserver!)
+            _timeObserver = nil
+        }
+        
+        //player.removeObserver(self, forKeyPath: "status", context: &myContext)
+        //player.removeObserver(self, forKeyPath: "loadedTimeRanges", context: &myContext)
+        //player.removeObserver(self, forKeyPath: "AVPlayerItemDidPlayToEndTimeNotification", context: &myContext)
+    }
+    
+    deinit {
+        stopObservers()
     }
 }
 
